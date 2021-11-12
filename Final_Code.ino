b5efcc0f62c1e527c78f46d8508933a863a84346
Md. Adnanul Haq <54657001+adnanshafi967@users.noreply.github.com>
2021-11-13 00:24:29 +0600

    Add files via upload

---
+++ Final_Code.ino
@@ -0,0 +1,42 @@
+#include "DHT.h"
+
+#define DHTPIN 2     
+#define DHTTYPE DHT11   
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("DHTxx test!"));
+
+  dht.begin();
+}
+
+void loop() {
+  
+  delay(500);
+
+  
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  
+ 
+  Serial.println(t);
+  Serial.println(h);
+
+
+}
